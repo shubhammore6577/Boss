a1572eef72c30722d45f29b1ca52f73ce8aa0c5b
Ganesh Raut <rautg95@gmail.com>
2022-09-19 15:49:03 +0530

    change done by GR second time

--- Github/src/com/github/pcg1/Main.java
+++ Github/src/com/github/pcg1/Main.java
@@ -5,6 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Up Sarpanch");
       System.out.println("Ganesh");
+      System.out.println("Up Sarpanch2");
+      System.out.println("Ganesh2");
 	}
 
 }
